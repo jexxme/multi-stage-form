09c35c52593e9b3510033c18d4fef2ee1a11dfa4
Jexxme <82029475+jexxme@users.noreply.github.com>
2023-11-17 13:19:41 +0100

    first working demo!

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-// This function will be called when moving to stage 2
 function goToStage2() {
     document.getElementById('stage1').style.display = 'none';
     document.getElementById('stage2').style.display = 'block';
-    // Update progress indicators
+    document.getElementById('backButton').style.display = 'inline-block'; // Show the back button
+    document.getElementById('nextButton').onclick = finishForm; // Change the event to finish form
+    document.getElementById('nextButton').textContent = 'Finish Now'; // Change the button text to 'Finish Now'
     updateProgressIndicator(1);
 }
 
-// This function will be called when the form is finished
-function finishForm() {
-    alert('Form Submitted!');
-    // Here you can add form submission logic
-    // Update progress indicators
-    updateProgressIndicator(2);
+function goToStage1() {
+    document.getElementById('stage1').style.display = 'block';
+    document.getElementById('stage2').style.display = 'none';
+    document.getElementById('backButton').style.display = 'none'; // Hide the back button
+    document.getElementById('nextButton').onclick = goToStage2; // Change the event back to go to stage 2
+    document.getElementById('nextButton').textContent = 'Next'; // Change the button text back to 'Next'
+    updateProgressIndicator(0);
 }
 
-// This function updates the progress indicators based on the current stage
+function finishForm() {
+    alert('Form Submitted!');
+    // Add form submission logic here
+    // Optionally, update the progress indicator to reflect form submission
+}
+
 function updateProgressIndicator(stage) {
     const indicators = document.querySelectorAll('.progress-indicators div');
     indicators.forEach((div, index) => {
