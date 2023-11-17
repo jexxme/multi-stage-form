42bd7b8bf2936545382e76f43e06e73fd3a127e0
Jexxme <82029475+jexxme@users.noreply.github.com>
2023-11-17 15:57:07 +0100

    indicator "fix"

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function goToStage1() {
+    // get the element with the id 'stage1' and add the class 'active'
     document.getElementById('stage1').classList.add('active');
+    // get the element with the id 'stage2' and remove the class 'active'
     document.getElementById('stage2').classList.remove('active');
+    // get the element with the id 'stage3' and remove the class 'active'
     document.getElementById('stage3').classList.remove('active');
+    // get the element with the id 'stage4' and remove the class 'active'
     document.getElementById('stage4').classList.remove('active');
+    // call the function updateProgressIndicator with the number 0
     updateProgressIndicator(0);
 }
-
 
 function goToStage2() {
     document.getElementById('stage1').classList.remove('active');
@@ -30,34 +34,51 @@ function goToStage4() {
     document.getElementById('stage4').classList.add('active');
     updateProgressIndicator(3);
 }
+
 function finishForm() {
-    // Hide all stages and buttons, but not the entire form
+    const progressIndicators = document.querySelector('.progress-indicators');
+    progressIndicators.classList.add('hide-progress');
+
+    // Hide all stages and buttons
     document.querySelectorAll('.form-stage').forEach(stage => stage.classList.remove('active'));
     document.querySelector('.button-container').style.display = 'none';
 
-    // Show the loading animation with fade-in
-    const loadingAnimation = document.getElementById('loadingAnimation');
-    loadingAnimation.classList.add('active');
-
-    // Simulate a 2-second submission process
+    // Wait for the hiding animation of progress indicators to complete
     setTimeout(() => {
-        // Hide the loading animation
-        loadingAnimation.classList.remove('active');
+        // Ensure progress indicators are completely hidden
+        progressIndicators.classList.add('hidden');
 
-        // Show the Thank You stage with fade-in
-        const thankYouStage = document.getElementById('thankYouStage');
-        thankYouStage.classList.add('active');
-    }, 2000); // 2000 milliseconds = 2 seconds
+        const loadingAnimation = document.getElementById('loadingAnimation');
+        loadingAnimation.classList.add('active');
+
+        // Simulate a 2-second submission process
+        setTimeout(() => {
+            loadingAnimation.classList.remove('active');
+            const thankYouStage = document.getElementById('thankYouStage');
+            thankYouStage.classList.add('active');
+        }, 2000);
+    }, 500);
 }
+
+
 
 
 function updateProgressIndicator(stage) {
-    const indicators = document.querySelectorAll('.progress-indicators div');
-    indicators.forEach((div, index) => {
-        if (index <= stage) {
-            div.classList.add('active');
-        } else {
-            div.classList.remove('active');
-        }
-    });
+
+// Find the progress indicator elements
+const indicators = document.querySelectorAll('.progress-indicators div');
+
+// Loop through each indicator element
+indicators.forEach((div, index) => {
+
+// If the element is before the stage we're at, mark it as active
+if (index <= stage) {
+div.classList.add('active');
+} else {
+
+// If the element is after the stage we're at, remove active
+div.classList.remove('active');
 }
+});
+}
+
